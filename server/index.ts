18f2c18fe4c9cae05aecb767f4694f79d9b2b5e3
clipwww <clipwww@gmail.com>
2019-02-25 17:47:14 +0800

    feat: add winston 紀錄Log

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import api from './api';
+import { expressWinstonLogger, expressWinstonErrorLogger } from '../utilities/logger.util';
 import { responseEndMiddleware } from './middlewares';
 
 const app = express();
 
+app.use(expressWinstonLogger);
+
 // Import API Routes
 app.use('/api', api, responseEndMiddleware);
+
+app.use(expressWinstonErrorLogger);
 
 export default app;
---
+++ utilities/logger.util.ts
@@ -0,0 +1,31 @@
+import winston from 'winston';
+import expressWinston from 'express-winston';
+import moment from 'moment';
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+  ],
+
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.colorize(),
+    winston.format.simple(),
+    winston.format.printf(info => {
+      return `[${info.level}] ${info.message} ${moment(info.timestamp).format('YYYY-MM-DD HH:mm:ss')} ${JSON.stringify(info.meta, null, 2)}`;
+    })
+  ),
+});
+
+export const expressWinstonLogger = expressWinston.logger({
+  expressFormat: true,
+  colorize: true,
+  meta: true,
+  winstonInstance: logger,
+});
+
+export const expressWinstonErrorLogger = expressWinston.errorLogger({
+  winstonInstance: logger,
+});
+
+export default logger;
